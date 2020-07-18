3c346d5fa4b7a7ebd34afbd11501cefd76792c3f
dongnan <dongnan@kedacom.com>
2020-07-18 22:29:30 +0800

    hystrix

---
+++ demo-springcloud/demo-springcloud-order/src/main/java/love/moon/remote/HelloHystrix.java
@@ -0,0 +1,12 @@
+package love.moon.remote;
+
+/**
+ * @author dongnan
+ * @date 2020/5/28 18:05
+ */
+public class HelloHystrix implements InventoryClient {
+    @Override
+    public String get() {
+        return null;
+    }
+}
--- demo-springcloud/demo-springcloud-order/src/main/java/love/moon/remote/InventoryClient.java
+++ demo-springcloud/demo-springcloud-order/src/main/java/love/moon/remote/InventoryClient.java
@@ -3,7 +3,7 @@ package love.moon.remote;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name= "spring-cloud-inventory")
+@FeignClient(name= "spring-cloud-inventory", fallback = HelloHystrix.class)
 public interface InventoryClient {
 
     @RequestMapping(value = "/inventory")
